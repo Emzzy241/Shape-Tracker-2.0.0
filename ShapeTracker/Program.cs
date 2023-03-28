24d84ffcafc35ac0509c758b162dd02d75c8c199
Emzzy241 <emzzyoluwole@gmail.com>
2023-03-28 06:12:12 +0100

    checking if my C# .ClearAll() setter method works

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -17,21 +17,30 @@ namespace ShapeTracker
             List<Triangle> allTriangles = Triangle.GetAll(); // first we call our Triangle.GetAll() method to get our static list of triangle objects.... There won't be any triangles in it yet at this time but that doesn't matter because the value of the allTriangles variable will update as we create new triangles
             Triangle testTriangle = new Triangle(3, 4, 5);
             Triangle secondTriangle = new Triangle(32, 74, 75);
-            
-            // a branchnto determine whether or not if our list of triangles is empty
+
+            // before the if-statement, we can update our UI logic to clear the triangles before our if statement using the beautiful Triangle.ClearAll() setter-method we wrote in our business logic
+            // This is used to check if my if-statement and my .ClearAll() setter-method actually works
+            // Triangle.ClearAll();
+            // Although I commented it out after I checked whether my if-statement and my .ClearAll() setter-method works
+
+            // a branch to determine whether or not if our list of triangles is empty
             // in the else-statement we handle all other cases i.e whern they are triangles in our list
-            if(allTriangles.Count == 0){
-                Console.WriteLine("There are mo triangles");
+            if (allTriangles.Count == 0)
+            {
+                Console.WriteLine("There are no triangles");
             }
-            else{
+            else
+            {
+                Console.WriteLine("Yay! you have triangles");
                 // the foreach I have here is saying: for each tri(a new variable that stores a triangle object) in allTriangles, I want to use my getter method(in business logic) to display all of the sides that exists in that particular triangle
-            // still in the else-statement if there are triangles, then we loop through the list and print information about each triangle
+                // still in the else-statement if there are triangles, then we loop through the list and print information about each triangle
                 Console.WriteLine("--------------------");
-                foreach(Triangle tri in allTriangles){
+                foreach (Triangle tri in allTriangles)
+                {
                     Console.WriteLine($"Side one of the triangle: {tri.GetSide1()}");
                     Console.WriteLine($"Side two of the triangle: {tri.GetSide2()}");
                     Console.WriteLine($"Side three of the triangle: {tri.GetSide3()}");
-                Console.WriteLine("--------------------");
+                    Console.WriteLine("--------------------");
                 }
             }
 
