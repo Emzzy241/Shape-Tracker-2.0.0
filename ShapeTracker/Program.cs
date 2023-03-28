3a29d96df09a097f913b95a83b3dec9ddb911f84
Emzzy241 <emzzyoluwole@gmail.com>
2023-03-28 05:45:57 +0100

    updating the C# UI logic file

--- ShapeTracker/Program.cs
+++ ShapeTracker/Program.cs
@@ -1,6 +1,6 @@
 using System;
 using ShapeTracker.Models; // where we told C# to make use of our Business Logic folder(Models)
-
+using System.Collections.Generic; // since I want to make use of my List of Triangle in this UI logic file too, I need to add the namespace that is housing C# Lists
 
 
 // Now to continue, we want to try using our class(which is in our Business logic folder[Models]) in our user Interface logic file(Program.cs)
@@ -12,63 +12,78 @@ namespace ShapeTracker
     {
         static void Main()
         {
-            // we are required to have a static void Main() method(within a class) to be the entry pointto our application
-            // Any code we add within the Main() method will be run when we run our console app
-            // It's convention to name the file and class that contains our app's entry point Program... Although you can choose not to name it Program.. I said its a common convention and not a mandatory convention
 
-            // Triangle testTriangle = new Triangle(); // here we created a new instance of the Triangleclass with the 
-            //                                         // we start by creating a variable: Triangle testTriangle, we then use the assignment operator = to set the testTriangle variable to a new Triangle object which we create using the new keyworda nd by invoking the Triangle() constructor method
-            // Console.WriteLine(testTriangle.GetType()); // then we get and print the type of testTriangle variable to the terminal using Console.WriteLine()
-
-            // To avoid us from having to write this entire 'ShapeTracker.Models.Triangle' to make .NET locate our Triangle
-            // We can easily tell C# to use our Business Logic Folder(Models) and to access our Triangle from that we use Trianle instead of ShapeTracker.Models.Triangle... That was when we did: using ShapeTracker.Models
-
-            // ACCESSING FIELDS:
-            // We want to access one of the firlds we have just created in our Business logic file(Triangle.cs)
-            // We have initially added a value to the Side2, all we have to do now is work with Side1 and Side3
-            // testTriangle.Side1 = 65;
-            // testTriangle.Side2 = 88;
-            // testTriangle.Side3 = 66;
-
-            // It is worthy to note that If I went ahead to do: testTriangle.Side2 = 48; O wpuld be overwriting the initial value I gave to Side2 in my Business logic file(and that initaila value was 4), WHat would be executed would be 48 now because C# has overwritten the previous value of 4
-            // Later on, we removed the initial value we gave to our Side2
-
-            // Console.WriteLine($"Side one of the Triangle: {testTriangle.Side1}");
-            // Console.WriteLine($"Side two of the Triangle: {testTriangle.Side2}");
-            // Console.WriteLine($"Side three of the Triangle: {testTriangle.Side3}");
-            // After adding my 3 fields, here are things to note:
-            // Similar to Javascript, we use dot notation to access fields, like testTriangle.Side1. Note that we cannot use brackets [] to access fields like we were able to do with Javascript properties. In C# brackets are used to apply indexing, like accessing an array element by its index location.
-            // Take note that all of the fields we created are instance fields, which means that we access them on an instance of the Triangle class, specifically testTriangle, and not the clas itself
-            // Within the new Console.WriteLine() statements, notice that we're using string interpolation to combine strings with variables
-            // Having done all of this, we run our Program with the dotnet run in the ShapeTracker project directory and we'll get our output that displays to us the 3 sides and the Object type(the line we used .GetType() method to know what type of object we were dealing with)
-
-            // When we call our constructor, we need to pass in an argument for each parameter, or else we'll get a compiler error.
+            // updating the entire code to work with the list of triangles(_instances) I now have in me business logic
+            List<Triangle> allTriangles = Triangle.GetAll();
             Triangle testTriangle = new Triangle(3, 4, 5);
+            Triangle secondTriangle = new Triangle(32, 74, 75);
 
-            // working with our getter methods in our UI
-            Console.WriteLine($"Side one of the Triangle: {testTriangle.GetSide1()}");
-            Console.WriteLine($"Side two of the Triangle: {testTriangle.GetSide2()}");
-            Console.WriteLine($"Side three of the Triangle: {testTriangle.GetSide3()}");
-            // Now I have successfully updated my code to make use of the constructor I have just created
 
-            // Calling my first Triangle instance method I wrote for Triangle that helps me check the type of a Trianle(whether it be scalene, isoceles, or equilateral)
-            Console.WriteLine("Want to know the type of triangle you have?");
-            Console.WriteLine($"Your triangle is: {testTriangle.CheckType()}");
 
-            // working with our setter methods in our UI
-            Console.WriteLine("Updating....");
-            testTriangle.SetSide1(44);
-            testTriangle.SetSide2(44);
-            testTriangle.SetSide3(70);
-            Console.WriteLine($"Actually, I've just changed the values of your triangle's sides to {testTriangle.GetSide1()}, {testTriangle.GetSide2()}, and {testTriangle.GetSide3()}.");
-            Console.WriteLine($"Now your triangle is: {testTriangle.CheckType()}");
-
-            // Now my Beautiful method gets called correctly
-            // NOTE: the CheckType() method is an instance method and it need to be called on an instance(example) of Triangle class and that is exactly what testTriangle variable is(an instance of a Triangle class)
-            
         }
     }
 }
+
+
+
+
+// THIS WAS THE BEGINN
+
+
+// we are required to have a static void Main() method(within a class) to be the entry pointto our application
+// Any code we add within the Main() method will be run when we run our console app
+// It's convention to name the file and class that contains our app's entry point Program... Although you can choose not to name it Program.. I said its a common convention and not a mandatory convention
+
+// Triangle testTriangle = new Triangle(); // here we created a new instance of the Triangleclass with the 
+//                                         // we start by creating a variable: Triangle testTriangle, we then use the assignment operator = to set the testTriangle variable to a new Triangle object which we create using the new keyworda nd by invoking the Triangle() constructor method
+// Console.WriteLine(testTriangle.GetType()); // then we get and print the type of testTriangle variable to the terminal using Console.WriteLine()
+
+// To avoid us from having to write this entire 'ShapeTracker.Models.Triangle' to make .NET locate our Triangle
+// We can easily tell C# to use our Business Logic Folder(Models) and to access our Triangle from that we use Trianle instead of ShapeTracker.Models.Triangle... That was when we did: using ShapeTracker.Models
+
+// ACCESSING FIELDS:
+// We want to access one of the firlds we have just created in our Business logic file(Triangle.cs)
+// We have initially added a value to the Side2, all we have to do now is work with Side1 and Side3
+// testTriangle.Side1 = 65;
+// testTriangle.Side2 = 88;
+// testTriangle.Side3 = 66;
+
+// It is worthy to note that If I went ahead to do: testTriangle.Side2 = 48; O wpuld be overwriting the initial value I gave to Side2 in my Business logic file(and that initaila value was 4), WHat would be executed would be 48 now because C# has overwritten the previous value of 4
+// Later on, we removed the initial value we gave to our Side2
+
+// Console.WriteLine($"Side one of the Triangle: {testTriangle.Side1}");
+// Console.WriteLine($"Side two of the Triangle: {testTriangle.Side2}");
+// Console.WriteLine($"Side three of the Triangle: {testTriangle.Side3}");
+// After adding my 3 fields, here are things to note:
+// Similar to Javascript, we use dot notation to access fields, like testTriangle.Side1. Note that we cannot use brackets [] to access fields like we were able to do with Javascript properties. In C# brackets are used to apply indexing, like accessing an array element by its index location.
+// Take note that all of the fields we created are instance fields, which means that we access them on an instance of the Triangle class, specifically testTriangle, and not the clas itself
+// Within the new Console.WriteLine() statements, notice that we're using string interpolation to combine strings with variables
+// Having done all of this, we run our Program with the dotnet run in the ShapeTracker project directory and we'll get our output that displays to us the 3 sides and the Object type(the line we used .GetType() method to know what type of object we were dealing with)
+
+// When we call our constructor, we need to pass in an argument for each parameter, or else we'll get a compiler error.
+// Triangle testTriangle = new Triangle(3, 4, 5);
+
+// working with our getter methods in our UI
+// Console.WriteLine($"Side one of the Triangle: {testTriangle.GetSide1()}");
+// Console.WriteLine($"Side two of the Triangle: {testTriangle.GetSide2()}");
+// Console.WriteLine($"Side three of the Triangle: {testTriangle.GetSide3()}");
+// Now I have successfully updated my code to make use of the constructor I have just created
+
+// Calling my first Triangle instance method I wrote for Triangle that helps me check the type of a Trianle(whether it be scalene, isoceles, or equilateral)
+// Console.WriteLine("Want to know the type of triangle you have?");
+// Console.WriteLine($"Your triangle is: {testTriangle.CheckType()}");
+
+// working with our setter methods in our UI
+// Console.WriteLine("Updating....");
+// testTriangle.SetSide1(44);
+// testTriangle.SetSide2(44);
+// testTriangle.SetSide3(70);
+// Console.WriteLine($"Actually, I've just changed the values of your triangle's sides to {testTriangle.GetSide1()}, {testTriangle.GetSide2()}, and {testTriangle.GetSide3()}.");
+// Console.WriteLine($"Now your triangle is: {testTriangle.CheckType()}");
+
+// Now my Beautiful method gets called correctly
+// NOTE: the CheckType() method is an instance method and it need to be called on an instance(example) of Triangle class and that is exactly what testTriangle variable is(an instance of a Triangle class)
+
 
 
 // As you can above, we have created our 'using System' directive in order to access the Console library so we can write to the terminal
